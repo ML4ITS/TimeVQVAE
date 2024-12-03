1825f58ec214a3735af1a41987efcad9e34c8063
danelee2601 <daesoolee2601@gmail.com>
2024-12-03 22:17:14 +0100

    separable conv

--- encoder_decoders/vq_vae_encdec.py
+++ encoder_decoders/vq_vae_encdec.py
@@ -21,10 +21,12 @@ class ResBlock(nn.Module):
 
         layers = [
             SnakeActivation(in_channels, 2), #SnakyGELU(in_channels, 2), #SnakeActivation(in_channels, 2), #nn.LeakyReLU(), #SnakeActivation(),
-            weight_norm(nn.Conv2d(in_channels, mid_channels, kernel_size=kernel_size, stride=(1, 1), padding=padding)),
+            weight_norm(nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=(1, 1), padding=padding, groups=in_channels)),
+            weight_norm(nn.Conv2d(in_channels, mid_channels, kernel_size=1)),
             nn.BatchNorm2d(out_channels),
             SnakeActivation(out_channels, 2), #SnakyGELU(out_channels, 2), #SnakeActivation(out_channels, 2), #nn.LeakyReLU(), #SnakeActivation(),
-            weight_norm(nn.Conv2d(mid_channels, out_channels, kernel_size=kernel_size, stride=(1, 1), padding=padding)),
+            weight_norm(nn.Conv2d(mid_channels, mid_channels, kernel_size=kernel_size, stride=(1, 1), padding=padding, groups=mid_channels)),
+            weight_norm(nn.Conv2d(mid_channels, out_channels, kernel_size=1)),
             nn.Dropout(dropout)
         ]
         self.convs = nn.Sequential(*layers)
@@ -47,7 +49,8 @@ class VQVAEEncBlock(nn.Module):
         padding = (0, 1) if frequency_indepence else (1, 1)
 
         self.block = nn.Sequential(
-            weight_norm(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding, padding_mode='replicate')),
+            weight_norm(nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding, padding_mode='replicate', groups=in_channels)),
+            weight_norm(nn.Conv2d(in_channels, out_channels, kernel_size=1)),
             nn.BatchNorm2d(out_channels),
             SnakeActivation(out_channels, 2), #SnakyGELU(out_channels, 2), #SnakeActivation(out_channels, 2), #nn.LeakyReLU(), #SnakeActivation(),
             nn.Dropout(dropout))
@@ -70,7 +73,8 @@ class VQVAEDecBlock(nn.Module):
         padding = (0, 1) if frequency_indepence else (1, 1)
 
         self.block = nn.Sequential(
-            weight_norm(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding)),
+            weight_norm(nn.ConvTranspose2d(in_channels, in_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding, groups=in_channels)),
+            weight_norm(nn.ConvTranspose2d(in_channels, out_channels, kernel_size=1)),
             nn.BatchNorm2d(out_channels),
             SnakeActivation(out_channels, 2), #SnakyGELU(out_channels, 2), #SnakeActivation(out_channels, 2), #nn.LeakyReLU(), #SnakeActivation(),
             nn.Dropout(dropout))
@@ -185,8 +189,12 @@ class VQVAEDecoder(nn.Module):
                 dec_layers.append(ResBlock(d, d, frequency_indepence, dropout=dropout))
             d //= 2
             dec_layers.append(VQVAEDecBlock(2*d, d, frequency_indepence))
-        dec_layers.append(nn.ConvTranspose2d(d, num_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding))
-        dec_layers.append(nn.ConvTranspose2d(num_channels, num_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding))
+        dec_layers.append(nn.Sequential(nn.ConvTranspose2d(d, d, kernel_size=kernel_size, stride=(1, 2), padding=padding, groups=d),
+                                        nn.ConvTranspose2d(d, num_channels, kernel_size=1)
+                                        ))
+        dec_layers.append(nn.Sequential(nn.ConvTranspose2d(num_channels, num_channels, kernel_size=kernel_size, stride=(1, 2), padding=padding),
+                                        nn.ConvTranspose2d(num_channels, num_channels, kernel_size=1)
+                                        ))
         self.decoder = nn.Sequential(*dec_layers)
 
         self.interp = nn.Upsample(input_length, mode='linear')
