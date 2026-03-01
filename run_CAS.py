6c25b81ee293f15f11419d37a4340f728fa77ee4
Daesoo Lee <daesoo@acondigital.com>
2026-03-01 10:42:24 +0100

    Migrate training and eval entrypoints into package modules
    - Move stage1, stage2, stage_neural_mapper, evaluate, and run_CAS logic into src/timevqvae
    - Replace repository-root scripts with thin compatibility wrappers that execute timevqvae.* modules
    - Keep existing python script commands working while aligning with src/ package conventions

--- run_CAS.py
+++ run_CAS.py
@@ -1,81 +1,9 @@
-"""
-CAS (= TSTR):
-- Train on the Synthetic samples, and
-- Test on the Real samples.
+from pathlib import Path
+import runpy
+import sys
 
-[1] Smith, Kaleb E., and Anthony O. Smith. "Conditional GAN for timeseries generation." arXiv preprint arXiv:2006.16477 (2020).
-"""
-from argparse import ArgumentParser
+SRC_DIR = Path(__file__).resolve().parent / "src"
+if str(SRC_DIR) not in sys.path:
+    sys.path.insert(0, str(SRC_DIR))
 
-import wandb
-import numpy as np
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-from pytorch_lightning.callbacks import LearningRateMonitor
-from pytorch_lightning.loggers import WandbLogger
-
-from preprocessing.preprocess_ucr import DatasetImporterUCR
-from preprocessing.data_pipeline import build_data_pipeline
-from utils import load_yaml_param_settings, get_root_dir, get_target_ucr_dataset_names, str2bool
-from evaluation.cas import CASDataset, ExpFCN
-
-
-def load_args():
-    parser = ArgumentParser()
-    parser.add_argument('--config', type=str, help="Path to the config data  file.", default=get_root_dir().joinpath('configs', 'config.yaml'))
-    parser.add_argument('--config_cas', type=str, help="Path to the config_cas data  file.", default=get_root_dir().joinpath('configs', 'config_cas.yaml'))
-
-    parser.add_argument('--dataset_names', nargs='+', help="e.g., Adiac Wafer Crop`.", default='')
-    parser.add_argument('--gpu_device_idx', type=int, help="GPU device index", default=0)
-    parser.add_argument('--use_neural_mapper', type=str2bool, default=False, help='Use the neural mapper')
-    parser.add_argument('--min_n_synthetic_train_samples', type=int, default=1000, help='It ensures a minimum number of a number of synthetic training set size to guarantee `y ∼ pθ(y|x) = pθ(x|y)p(y)/pθ(x)`.')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    # load config
-    args = load_args()
-    config = load_yaml_param_settings(args.config)
-    config_cas = load_yaml_param_settings(args.config_cas)
-    
-    device = args.gpu_device_idx
-    batch_size = config_cas['dataset']['batch_size']
-
-    # run
-    dataset_names = get_target_ucr_dataset_names(args)
-    print('dataset_names:', dataset_names)
-    for dataset_name in dataset_names:
-        # data pipeline
-        dataset_importer = DatasetImporterUCR(dataset_name, **config['dataset'])
-        real_train_data_loader, real_test_data_loader = [build_data_pipeline(batch_size, dataset_importer, config, kind) for kind in ['train', 'test']]
-        synthetic_data_loader = DataLoader(
-            CASDataset(real_train_data_loader,
-                       args.min_n_synthetic_train_samples,
-                       dataset_name,
-                       config,
-                       device,
-                       args.use_neural_mapper),
-            batch_size=batch_size,
-            num_workers=config_cas['dataset']['num_workers'],
-            shuffle=True,
-            drop_last=False)
-
-        # fit
-        wandb_logger = WandbLogger(project='TimeVQVAE-CAS', 
-                                   name=dataset_name, 
-                                   config={**config, **config_cas, 'device':device, 'dataset_name':dataset_name})
-        n_classes = len(np.unique(real_train_data_loader.dataset.Y))
-        train_exp = ExpFCN(config_cas, n_classes)
-        trainer = pl.Trainer(logger=wandb_logger,
-                             enable_checkpointing=False,
-                             callbacks=[LearningRateMonitor(logging_interval='step')],
-                             max_steps=config_cas['trainer_params']['max_steps'],
-                             devices=[device,],
-                             accelerator='gpu',
-                             val_check_interval=config_cas['trainer_params']['val_check_interval'],
-                             check_val_every_n_epoch=None,)
-        trainer.fit(train_exp,
-                    train_dataloaders=synthetic_data_loader,
-                    val_dataloaders=real_test_data_loader, )
-
-        wandb.finish()
+runpy.run_module("timevqvae.run_CAS", run_name="__main__")
---
+++ src/timevqvae/stage1.py
@@ -0,0 +1,105 @@
+"""
+Stage 1: VQ training
+
+run `python stage1.py`
+"""
+import os
+from argparse import ArgumentParser
+import copy
+
+import torch
+import wandb
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.loggers import WandbLogger
+from torch.utils.data import DataLoader
+
+from timevqvae.preprocessing.preprocess_ucr import DatasetImporterUCR, DatasetImporterCustom
+from timevqvae.experiments.exp_stage1 import ExpStage1
+from timevqvae.preprocessing.data_pipeline import build_data_pipeline, build_custom_data_pipeline
+from timevqvae.utils import get_root_dir, load_yaml_param_settings, str2bool
+
+
+def load_args():
+    parser = ArgumentParser()
+    parser.add_argument('--config', type=str, help="Path to the config data  file.",
+                        default=get_root_dir().joinpath('configs', 'config.yaml'))
+    parser.add_argument('--dataset_names', nargs='+', help="e.g., Adiac Wafer Crop`.", default='')
+    parser.add_argument('--gpu_device_ind', nargs='+', default=[0], type=int, help='Indices of GPU devices to use.')
+    parser.add_argument('--use_custom_dataset', type=str2bool, default=False, help='Using a custom dataset, then set it to True.')
+    return parser.parse_args()
+
+
+def train_stage1(config: dict,
+                 dataset_name: str,
+                 train_data_loader: DataLoader,
+                 test_data_loader: DataLoader,
+                 gpu_device_ind,
+                 ):
+    """
+    :param do_validate: if True, validation is conducted during training with a test dataset.
+    """
+    project_name = 'TimeVQVAE-stage1'
+
+    # fit
+    _, in_channels, input_length = train_data_loader.dataset.X.shape
+    train_exp = ExpStage1(in_channels, input_length, config)
+    
+    n_trainable_params = sum(p.numel() for p in train_exp.parameters() if p.requires_grad)
+    wandb_logger = WandbLogger(project=project_name, name=None, config={**config, 'dataset_name': dataset_name, 'n_trainable_params:': n_trainable_params})
+
+    # Check if GPU is available
+    if not torch.cuda.is_available():
+        print('GPU is not available.')
+        # num_cpus = multiprocessing.cpu_count()
+        num_cpus = 1
+        print(f'using {num_cpus} CPUs..')
+        device = num_cpus
+        accelerator = 'cpu'
+    else:
+        accelerator = 'gpu'
+        device = gpu_device_ind
+        
+    trainer = pl.Trainer(logger=wandb_logger,
+                         enable_checkpointing=False,
+                         callbacks=[LearningRateMonitor(logging_interval='step')],
+                         max_steps=config['trainer_params']['max_steps']['stage1'],
+                         devices=device,
+                         accelerator=accelerator,
+                         val_check_interval=config['trainer_params']['val_check_interval']['stage1'],
+                         check_val_every_n_epoch=None,
+                         # precision='bf16',
+                         accumulate_grad_batches=1,
+                         )
+    trainer.fit(train_exp,
+                train_dataloaders=train_data_loader,
+                val_dataloaders=test_data_loader
+                )
+
+    # test
+    print('closing...')
+    wandb.finish()
+
+    print('saving the models...')
+    if not os.path.isdir(get_root_dir().joinpath('saved_models')):
+            os.mkdir(get_root_dir().joinpath('saved_models'))
+    trainer.save_checkpoint(os.path.join(f'saved_models', f'stage1-{dataset_name}.ckpt'))
+
+
+if __name__ == '__main__':
+    # load config
+    args = load_args()
+    config = load_yaml_param_settings(args.config)
+
+    for dataset_name in args.dataset_names:
+        # data pipeline
+        batch_size = config['dataset']['batch_sizes']['stage1']
+        if not args.use_custom_dataset:
+            dataset_importer = DatasetImporterUCR(dataset_name, **config['dataset'])
+            train_data_loader, test_data_loader = [build_data_pipeline(batch_size, dataset_importer, config, kind) for kind in ['train', 'test']]
+        else:
+            dataset_importer = DatasetImporterCustom(**config['dataset'])
+            train_data_loader, test_data_loader = [build_custom_data_pipeline(batch_size, dataset_importer, config, kind) for kind in ['train', 'test']]
+
+        # train
+        train_stage1(config, dataset_name, train_data_loader, test_data_loader, args.gpu_device_ind)
